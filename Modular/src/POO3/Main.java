958a04852081725d0388e4986650e75b3981db7e
itsjacobs <jacob.rosales@educa.madrid.org>
2025-03-20 21:49:12 +0100

    Delete Modular directory

--- Modular/src/POO3/Main.java
+++
@@ -1,9 +0,0 @@
-package POO3;
-
-public class Main {
-    public static void main(String[] args) {
-        Titular titular = new Titular("Marta","Gomez",20);
-        CuentaCorriente cc = new CuentaCorriente(titular,"ES57832847328432",35.5);
-        
-    }
-}
